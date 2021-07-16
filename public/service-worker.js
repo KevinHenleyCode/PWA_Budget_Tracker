2644c443709c6d8f1dcbfeab2134daaf3093ad6d
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-15 21:56:29 -0500

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,50 @@
-console.log('This is the service worker!');
+const runTime = 'runtime'
+const preCache = 'precache-v1'
+const cacheFiles = [
+  '/',
+  'index.js',
+  'index.html',
+  'style.css',
+]
+
+
+console.log('Service worker up and running!');
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(preCache).then((cache) => cache.addAll(cacheFiles)).then(self.skipWaiting()))
+})
+
+
+self.addEventListener('activate', (e) => {
+  const currentCaches = [preCache, runTime]
+  
+  e.waitUntil(caches.keys().then((cacheNames) => {
+        
+    return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))})
+    .then((cachesToDelete) => {
+
+    return Promise.all(cachesToDelete.map((cacheToDelete) => {return caches.delete(cacheToDelete)}))})
+    .then(() => self.clients.claim()))
+})
+
+
+self.addEventListener('fetch', (e) => {
+  if (e.req.url.startsWith(self.location.origin)) {
+    e.respondWith(caches.match(e.req).then((res) => {
+        
+        if (res) {
+          return response;
+        }
+
+        return caches.open(runTime).then((cache) => {
+          return fetch(e.req).then((res) => {
+            return cache.put(e.req, res.clone()).then(() => {
+              return res
+            })
+          })
+        })
+      })
+    )
+  }
+})
