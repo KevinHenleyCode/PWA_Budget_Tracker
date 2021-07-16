f667189e4846f3ba4dd37b945479d037690bbbb3
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-16 00:45:04 -0500

    made a new service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,4 @@
-const runTime = 'runtime'
-const preCache = 'precache-v1'
+const cacheName = 'v1'
 const cacheFiles = [
   '/',
   'index.js',
@@ -8,43 +7,63 @@ const cacheFiles = [
 ]
 
 
-console.log('Service worker up and running!');
+self.addEventListener('install', function(e) {
+    console.log('Service worker is working');
 
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(preCache).then((caches) => caches.addAll(cacheFiles)).then(self.skipWaiting()))
-})
-
-
-self.addEventListener('activate', (e) => {
-  const currentCaches = [preCache, runTime]
-  
-  e.waitUntil(caches.keys().then((cacheNames) => {
-        
-    return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))})
-    .then((cachesToDelete) => {
-
-    return Promise.all(cachesToDelete.map((cacheToDelete) => {return caches.delete(cacheToDelete)}))})
-    .then(() => self.clients.claim()))
-})
-
-
-self.addEventListener('fetch', (e) => {
-  if (e.req.url.startsWith(self.location.origin)) {
-    e.respondWith(caches.match(e.req).then((res) => {
-        
-        if (res) {
-          return response;
-        }
-
-        return caches.open(runTime).then((cache) => {
-          return fetch(e.req).then((res) => {
-            return cache.put(e.req, res.clone()).then(() => {
-              return res
-            })
-          })
+    e.waitUntil(
+        caches.open(cacheName)
+        .then(function (cache) {  
+            console.log('Caching files')
+            return cache.addAll(cacheFiles)
         })
-      })
     )
-  }
+})
+
+self.addEventListener('acrive', function(e) {
+    console.log('Service worker is now activated')
+
+    e.waitUntil(
+        caches.keys()
+        .then(function (cacheNames) {  
+            return Promise.all(cacheNames.map(function (thisCacheName) {
+                if (thisCacheName !== cacheName) {
+                    console.log('Removing files from cache')
+                    return caches.delete(thisCacheName)
+                } 
+            }))
+        })
+    )
+})
+
+self.addEventListener('fetch', function (e) {
+    console.log('Service worker is now fetching', e.request.url)
+    e.respondWith(
+        caches.match(e.request)
+        .then(function(response){
+            if (response) {
+                console.log(e.request.url)
+                return response
+            }
+
+            const reqClone = e.request.clone()
+
+            fetch(reqClone)
+            .then(function (responce) {  
+                if (!responce) {
+                    console.log('fetch has not responded')
+                    return responce
+                }
+
+                const respClone = responce.clone()
+
+                caches.open(cacheName).then(function(cache){
+                    cache.put(e.request, respClone)
+                    return responce
+                })
+            })
+            .catch(function(error) {
+                console.log('There was an error', error);
+            })
+        })
+    )
 })
