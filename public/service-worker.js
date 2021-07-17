2a0b6150be78a910571171846d7c36ecfb69db96
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-17 00:53:01 -0500

    cleaned up script

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,65 +14,44 @@ const FILES_TO_CACHE = [
 console.log('The Service Worker is up and running!');
 
 
-// install
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Files pre-cached!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
+      console.log("Files pre-cached!")
+      return cache.addAll(FILES_TO_CACHE)
+    }))
   self.skipWaiting();
-});
+})
 
 self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
+  evt.waitUntil(caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Deleting cache", key);
-            return caches.delete(key);
+            console.log("Deleting cache", key)
+            return caches.delete(key)
           }
         })
-      );
+      )
     })
-  );
+  )
+  self.clients.claim()
+})
 
-  self.clients.claim();
-});
-
-// fetch
 self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
   if (evt.request.url.includes("/api/transaction")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
+    evt.respondWith(caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request).then(response => {
+            if (response.status === 200) {cache.put(evt.request.url, response.clone())}
+            return response
+          }).catch(err => {
             return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
-    );
-
-    return;
+          })
+      }).catch(err => console.log(err)))
+    return
   }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.match(evt.request).then(function(response) {
+  evt.respondWith(caches.match(evt.request).then(function(response) {
       return response || fetch(evt.request);
     })
-  );
-});
+  )
+})
