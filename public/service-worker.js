ea2632354853b24d4e35f092ae21df8ac327381c
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-17 00:37:03 -0500

    fixed styles.css not being cached because it was the wrong name

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,81 +1,77 @@
-const runTime = 'runtime'
-const preCache = 'precache-v1'
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 
-const cacheFiles = [
+const FILES_TO_CACHE = [
   '/',
   'index.js',
   'index.html',
-  'style.css',
+  'styles.css',
 ]
 
 
 console.log('The Service Worker is up and running!');
 
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(preCache).then((caches) => caches.addAll(cacheFiles)).then(self.skipWaiting()))
-})
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Files pre-cached!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
+  self.skipWaiting();
+});
 
-self.addEventListener('activate', (e) => {
-  const currentCaches = [preCache, runTime]
-  e.waitUntil(caches.keys().then((cacheNames) => {
-    return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))}).then((cachesToDelete) => {
-    return Promise.all(cachesToDelete.map((cacheToDelete) => {return caches.delete(cacheToDelete)}))}).then(() => self.clients.claim()))
-})
-
-
-self.addEventListener('fetch', (e) => {
-  if (e.req.url.startsWith(self.location.origin)) {
-    e.respondWith(caches.match(e.req).then((res) => {
-        
-        if (res) {
-          return response;
-        }
-
-        return caches.open(runTime).then((cache) => {
-          return fetch(e.req).then((res) => {
-            return cache.put(e.req, res.clone()).then(() => {
-              return res
-            })
-          })
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Deleting cache", key);
+            return caches.delete(key);
+          }
         })
-      })
-    )
-  }
-})
+      );
+    })
+  );
 
+  self.clients.claim();
+});
 
-self.addEventListener('fetch', function (e) {
-    console.log('Service worker is now fetching', e.request.url)
-    e.respondWith(caches.match(e.request).then(function(response){
-            
-            if (response) {
-                console.log(e.request.url)
-                return response
+// fetch
+self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/transaction")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
             }
 
-            const reqClone = e.request.clone()
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
 
-            fetch(reqClone).then(function (responce) {  
-                
-                if (!responce) {
-                    console.log('fetch has not responded')
-                    return responce
-                }
+    return;
+  }
 
-                const respClone = responce.clone()
-
-                caches.open(cacheName).then(function(cache){
-                    cache.put(e.request, respClone)
-                    return responce
-                })
-            })
-            
-            .catch(function(error) {
-                console.log('There was an error', error);
-            })
-        })
-    )
-})
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
